8743ef08c74139898c980fbc1a5f448accdd8c4d
Robbie Tilton <rtilton1@gmail.com>
2012-10-20 19:26:15 -0400

    renamed the folder name and finessed the click and drag motion

---
+++ Week6/double-matrix-notes.js
@@ -0,0 +1,45 @@
+	//attempt at double matrix
+	//[0][0] first element in first array
+	//[0][1] second element in first array
+	//[1][0] first element in the second array
+	//[1][1] second element in second array
+	// window.matrix = []
+
+	// var currPos = Array(2); //an array with 2 slots
+	// currPos[0] = new THREE.Vector3( -100, 100, 0 );
+	// currPos[1] = new THREE.Vector3( -100, -100, 0 );
+	// matrix.push(currPos);
+	
+	// var currPos2 = Array(2);
+	// currPos2[0] = new THREE.Vector3( -200, 100, 0 );
+	// currPos2[1] = new THREE.Vector3( -200, -100, 0 );
+	// matrix.push(currPos2);
+
+	// var currPos3 = Array(3); //an array with 2 slots
+	// currPos3[0] = new THREE.Vector3( -100, 100, 0 );
+	// currPos3[1] = new THREE.Vector3( -100, -100, 0 );
+	// matrix.push(currPos);
+
+	// var currPos4 = Array(2);
+	// currPos4[0] = new THREE.Vector3( -200, 100, 0 );
+	// currPos4[1] = new THREE.Vector3( -200, -100, 0 );
+	// matrix.push(currPos4);
+	
+	// var geometry = new THREE.Geometry();
+	// for (var i = 0; i <4; i++){
+	//   var tacka1 = new THREE.Vector3();
+	//   tacka1.x = matrix[i][0];
+	//   tacka1.y = 0;
+	//   tacka1.z = matrix[i][1];
+
+	//   var tacka2 = new THREE.Vector3();
+	//   tacka2.x = matrix[i][0];
+	//   tacka2.y = 20;
+	//   tacka2.z = matrix[i][1];
+
+	//   geometry.vertices.push(tacka1);
+	//   geometry.vertices.push(tacka2);
+	// }
+	// var ribbon = new THREE.MeshBasicMaterial({ color: 0xFF0000, wireframe: true });
+	// ribbon.doubleSided = true;
+	// group.add(ribbon);
